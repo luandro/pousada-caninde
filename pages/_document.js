5725e64249e30e61d33ac0da336fedec49276435
Diego M. Rodrigues <diegomr86@gmail.com>
2017-03-07 20:46:07 -0300

    init

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+import flush from 'styled-jsx/server'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const {html, head} = renderPage()
+    const styles = flush()
+    return { html, head, styles }
+  }
+
+  render () {
+    return (
+     <html>
+       <Head>
+         <style>{`
+          body { 
+          } /* custom! */
+          `}</style>
+       </Head>
+       <body className="custom_class">
+         {this.props.customValue}
+         <Main />
+         <NextScript />
+       </body>
+     </html>
+    )
+  }
+}
