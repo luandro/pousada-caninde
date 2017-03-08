e3329a184f123a89a4892cc121f03e0a782ce558
luandro <luandro@gmail.com>
2017-03-08 09:41:30 -0300

    Fixed map, added marker

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,7 @@
 import GoogleMapReact from 'google-map-react'
+import Icon from 'react-icons/lib/md/place'
+
+const Marker = () => <Icon size={85} color={'#005FB6'} />
 
 const Map = ({ center, zoom }) => (
     <div className="wrapper">
@@ -6,6 +9,7 @@ const Map = ({ center, zoom }) => (
             defaultCenter={center}
             defaultZoom={zoom}
         >
+            <Marker lat={-14.1754559} lng={-47.8153361} />
         </GoogleMapReact>
         <style jsx>{`
             .wrapper {
@@ -18,7 +22,7 @@ const Map = ({ center, zoom }) => (
 
 Map.defaultProps = {
     center: {lat: -14.1769177, lng: -47.8143979},
-    zoom: 17.5
+    zoom: 15
 }
 
 export default Map;
